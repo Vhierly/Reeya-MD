22cb914f0e8095958061c6aa19e8a321e6e8df69
Vhierly <109750625+Vhierly@users.noreply.github.com>
2022-08-13 11:07:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ global.autoReadAll = false
 global.antitags = true
 
 
-global.Owner = ['918101187835']
-global.OwnerNumber = ['918101187835']
-global.ownertag = ['918101187835']
-global.BotName = "Miku"
-global.packname = "Miku bot"
-global.author = "By: Fantox"
-global.OwnerName = "Fantox"
-global.BotSourceCode = "https://github.com/FantoX001/Miku-MD"
+global.Owner = ['16028253490']
+global.OwnerNumber = ['16028253490']
+global.ownertag = ['16028253490']
+global.BotName = "ReeyaBOAT"
+global.packname = "Reeya Boat"
+global.author = "By: Secret xD "
+global.OwnerName = "Vhierly"
+global.BotSourceCode = "https://github.com/Vhierly/Reeya-MD"
 global.SupportGroupLink = "https://chat.whatsapp.com/JcBzjRIxM5UHfuJbt0KrWt"
 global.sessionName = "session"
 
@@ -23,12 +23,12 @@ global.sessionName = "session"
 global.prefa = ['-'] 
 
 
-global.location = "West Bengal, India"
+global.location = "Indo Pride 💖 "
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
-global.websitex = "https://github.com/FantoX001"
-global.lolhuman = "KaysaS"
+global.websitex = "https://github.com/Vhierly/"
+global.lolhuman = "Created by Reeya with <3"
 
 
 global.BotLogo = fs.readFileSync("./Assets/pic1.jpg")
